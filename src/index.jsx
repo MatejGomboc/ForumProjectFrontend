64b04490bfcf4456b92845afc88b9b8ba285b03c
Matej Gomboc <matejg03@gmail.com>
2022-06-19 16:06:50 +0200

    Fixed routing.

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={process.env.ROOT_LOCATION}>
+        <BrowserRouter basename={process.env.REACT_APP_ROOT_LOCATION}>
             <Suspense fallback={<LoadingPage />}>
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
